5b6c03b1f1a57c2f9fbe742155fc066fcf40a785
gose <gose@goseui-MacBook-Pro.local>
2018-07-03 23:08:08 +0900

    Add vue, parcel 설치

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <app-map></app-map>
+  </div>
+</template>
+
+<script>
+import AppMap from "./components/map";
+
+export default {
+  name: 'app',
+  components: {
+      AppMap
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  #app {
+    color: #56b983;
+  }
+</style>
---
+++ src/components/map.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-map',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App!'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  #app {
+    color: #56b983;
+  }
+</style>
